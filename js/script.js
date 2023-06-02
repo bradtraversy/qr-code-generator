ab7942e86ceb7ad395e0667346799e40a91fb578
Hardikk Kamboj <64458111+hady68@users.noreply.github.com>
2023-06-02 13:32:27 +0530

    BUG : Image was not getting Downloaded in mobile
    Added the code that fixes the issue for mobile where the whole HTML page was getting downloaded instead of the image itself

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const form = document.getElementById('generate-form');
-const qr = document.getElementById('qrcode');
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
 
 // Button submit
 const onGenerateSubmit = (e) => {
@@ -7,23 +7,23 @@ const onGenerateSubmit = (e) => {
 
   clearUI();
 
-  const url = document.getElementById('url').value;
-  const size = document.getElementById('size').value;
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
 
   // Validate url
-  if (url === '') {
-    alert('Please enter a URL');
+  if (url === "") {
+    alert("Please enter a URL");
   } else {
     showSpinner();
     // Show spinner for 1 sec
     setTimeout(() => {
       hideSpinner();
       generateQRCode(url, size);
-
+      showScanner();
       // Generate the save button after the qr code image src is ready
       setTimeout(() => {
         // Get save url
-        const saveUrl = qr.querySelector('img').src;
+        const saveUrl = qr.querySelector("canvas").toDataURL();
         // Create save button
         createSaveBtn(saveUrl);
       }, 50);
@@ -33,7 +33,7 @@ const onGenerateSubmit = (e) => {
 
 // Generate QR code
 const generateQRCode = (url, size) => {
-  const qrcode = new QRCode('qrcode', {
+  const qrcode = new QRCode("qrcode", {
     text: url,
     width: size,
     height: size,
@@ -42,37 +42,45 @@ const generateQRCode = (url, size) => {
 
 // Clear QR code and save button
 const clearUI = () => {
-  qr.innerHTML = '';
-  const saveBtn = document.getElementById('save-link');
+  qr.innerHTML = "";
+  const saveBtn = document.getElementById("save-link");
   if (saveBtn) {
     saveBtn.remove();
   }
 };
 
+// hide  scanner
+const showScanner = () => {
+  const scanner = document.getElementById("qrCodeContainer");
+  scanner.style.display = "block";
+};
+
 // Show spinner
 const showSpinner = () => {
-  const spinner = document.getElementById('spinner');
-  spinner.style.display = 'block';
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "block";
 };
 
 // Hide spinner
 const hideSpinner = () => {
-  const spinner = document.getElementById('spinner');
-  spinner.style.display = 'none';
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
 };
 
 // Create save button to download QR code as image
 const createSaveBtn = (saveUrl) => {
-  const link = document.createElement('a');
-  link.id = 'save-link';
+  const link = document.createElement("a");
+  link.id = "save-link";
   link.classList =
-    'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    "bg-green-500 hover:bg-blue-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+  link.innerHTML = "Save Image";
+
   link.href = saveUrl;
-  link.download = 'qrcode';
-  link.innerHTML = 'Save Image';
-  document.getElementById('generated').appendChild(link);
+  link.download = "qrcode.png";
+
+  document.getElementById("generated").appendChild(link);
 };
 
 hideSpinner();
 
-form.addEventListener('submit', onGenerateSubmit);
+form.addEventListener("submit", onGenerateSubmit);
